bc87589ecf073bebb9b2e2a1d4929f2723858ef9
Rochman Kaji <aldhinya.aldhi@yahoo.com>
2018-05-24 20:53:19 +0700

    Create sequential-search.cpp

---
+++ Searching/sequential-search.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int arr1['n'];
+    int cari;
+    int n;
+    bool ditemukan;
+    int lokasi;
+
+    cout <<"> Input Batas = ";
+    cin>>n;
+
+    for(int i=0; i<n; i++)
+    {
+        cout <<"Nilai ke ["<<i+1<<"] = ";
+        cin>>arr1[i];
+    }
+
+    cout <<endl;
+
+    do{
+    cout <<"> Input nilai yang ingin dicari = ";
+    cin>>cari;
+    cout <<"\n> Proses Pencarian : ";
+    for(int i=0; i<n; i++)
+    {
+        cout <<"\nNilai ke ["<<i+1 << "]";
+        if(arr1[i]==cari)
+        {
+            ditemukan = true;
+            cout <<" = Ketemu!";
+            lokasi=i;
+        }
+    }
+    cout << endl <<endl;
+    if(ditemukan)
+    {
+        cout <<"> Nilai ditemukan pada indeks array ke ["<<lokasi+1 <<"]." <<endl <<endl;
+    }
+    else
+    {
+        cout <<"> ERROR: Nilai ["<<cari<<"] Tidak ditemukan."<<endl <<endl;
+    }
+    }while(true);
+    return 0;
+}
